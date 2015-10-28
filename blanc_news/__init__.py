d8587a459812b0a4d5e665e5caacc01d45a0aa8f
Edvinas Jurevicius <ikonitas@gmail.com>
2015-10-28 15:19:03 +0000

    Add rquired files

---
+++ blanc_news/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import apps as django_apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+default_app_config = 'blanc_news.apps.BlancNewsConfig'
+
+
+def get_post_model():
+    try:
+        conf_model = getattr(settings, 'NEWS_POST_MODEL', 'news.Post')
+        return django_apps.get_model(conf_model)
+    except ValueError:
+        raise ImproperlyConfigured("NEWS_POST_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured("NEWS_POST_MODEL refers to model '%s' that has not been installed" % conf_model)
---
+++ blanc_news/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class BlancNewsConfig(AppConfig):
+    name = 'blanc_news'
+    label = 'news'
