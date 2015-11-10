34d6ce33c0d51e50b40e737c2e32f7425bb2a82b
Edvinas Jurevicius <ikonitas@gmail.com>
2015-11-10 14:56:23 +0000

    Add search indexes

--- blanc_news/mixins.py
+++ blanc_news/mixins.py
@@ -6,4 +6,3 @@ from .models import Post
 class PostMixin(object):
     def get_queryset(self):
         return Post.objects.select_related().filter(published=True).exclude(current_version=None)
-
---
+++ blanc_news/search_indexes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from haystack import indexes
+
+from .models import Post
+
+
+class PostIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Post
