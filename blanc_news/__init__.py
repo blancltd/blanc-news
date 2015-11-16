6050782f7f4580957d8f1641ccda9f3af0b818d9
Alex Tomkins <tomkins@darkzone.net>
2015-11-16 14:29:45 +0000

    We're not using swappable models

--- blanc_news/__init__.py
+++ blanc_news/__init__.py
@@ -1,16 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from django.apps import apps as django_apps
-from django.core.exceptions import ImproperlyConfigured
-
-
 default_app_config = 'blanc_news.apps.BlancNewsConfig'
-
-
-def get_post_model():
-    try:
-        return django_apps.get_model('blanc_news.Post')
-    except ValueError:
-        raise ImproperlyConfigured(
-            "Blanc news  must be of the form 'app_label.model_name'"
-        )
--- blanc_news/templatetags/news_tags.py
+++ blanc_news/templatetags/news_tags.py
@@ -3,8 +3,7 @@
 from django import template
 from django.utils import timezone
 
-from blanc_news import get_post_model
-from blanc_news.models import Category
+from blanc_news.models import Category, Post
 
 register = template.Library()
 
@@ -16,13 +15,13 @@ def get_news_categories():
 
 @register.assignment_tag
 def get_news_months():
-    return get_post_model().objects.filter(
+    return Post.objects.filter(
         published=True, date__lte=timezone.now()).dates('date_url', 'month')
 
 
 @register.assignment_tag
 def get_latest_news(count, category=None):
-    post_list = get_post_model().objects.select_related().filter(
+    post_list = Post.objects.select_related().filter(
         published=True, date__lte=timezone.now())
 
     # Optional filter by category
@@ -30,4 +29,3 @@ def get_latest_news(count, category=None):
         post_list = post_list.filter(category__slug=category)
 
     return post_list[:count]
-
