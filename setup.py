d363631106c95365e138bea08ca0f8811c1dba67
Alex Tomkins <tomkins@darkzone.net>
2016-04-13 12:40:33 +0100

    Update python versions we care about

--- setup.py
+++ setup.py
@@ -23,8 +23,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     license='BSD 3-Clause',
 )
