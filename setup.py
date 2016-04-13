4135873b263ab4f5a9e7c8eb3d41e07eefcb2ac0
Alex Tomkins <tomkins@darkzone.net>
2016-04-13 12:41:14 +0100

    Minor setup.py improvements

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from codecs import open
+
+from setuptools import find_packages, setup
+
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
 
 setup(
     name='django-glitter-news',
     version='0.1',
     description='Django Glitter News for Django',
-    long_description=open('README.rst').read(),
+    long_description=readme,
     url='https://github.com/blancltd/django-glitter-news',
     maintainer='Blanc Ltd',
     maintainer_email='studio@blanc.ltd.uk',
@@ -27,5 +34,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    license='BSD 3-Clause',
+    license='BSD',
 )
