8bb8f1b17b00d60214dc2f41d8f08ebd629d6760
martinphellwig <martin.hellwig@gmail.com>
2017-02-01 13:24:46 +0000

    readability improvement

--- glitter_news/templatetags/news_tags.py
+++ glitter_news/templatetags/news_tags.py
@@ -26,11 +26,11 @@ def get_latest_news(count, category='', tags=''):
     post_list = Post.objects.published().select_related().filter(date__lte=timezone.now())
 
     # Optional filter by category
-    if category.strip() is not '':
+    if not category.strip():
         post_list = post_list.filter(category__slug=category)
     
     # Optional filter by tags
-    if tags.strip() is not '': 
+    if not tags.strip(): 
         post_list = post_list.filter(tags__name__in=utils.parse_tags(tags))
 
     return post_list[:count]
