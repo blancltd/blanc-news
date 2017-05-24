d1ce71fb8defb501d3c3cf62ba757b172eb3f674
Tom Smith <tom@takeontom.com>
2017-05-24 11:25:43 +0100

    Tweaks to code to make it format better with yapf
    For #21

--- glitter_news/glitter_apps.py
+++ glitter_news/glitter_apps.py
@@ -2,9 +2,5 @@ from glitter.integration import GlitterApp
 
 apps = {
     'glitter_news':
-        GlitterApp(
-            name='Glitter News',
-            url_conf='glitter_news.urls',
-            namespace='glitter-news',
-        ),
+        GlitterApp(name='Glitter News', url_conf='glitter_news.urls', namespace='glitter-news')
 }
